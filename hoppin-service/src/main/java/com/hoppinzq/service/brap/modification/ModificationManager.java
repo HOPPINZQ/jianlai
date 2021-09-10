6a018b487ba17fbc25b97bb6c3719ae9c8022f7e
zhangqi <anmiezata@163.com>
2021-09-10 16:41:02 +0800

    删除服务

--- hoppin-service/src/main/java/com/hoppinzq/service/brap/modification/ModificationManager.java
+++
@@ -1,9 +0,0 @@
-package com.hoppinzq.service.brap.modification;
-
-
-import com.hoppinzq.service.brap.common.ModificationList;
-
-public interface ModificationManager {
-    public Object[] applyModificationScheme(Object[] objects);
-    public ModificationList[] getModifications();
-}
--- hoppin-service/src/main/java/com/hoppinzq/service/server/TestService.java
+++
@@ -1,5 +0,0 @@
-package com.hoppinzq.service.server;
-
-public interface TestService {
-    public String test(String str);
-}
