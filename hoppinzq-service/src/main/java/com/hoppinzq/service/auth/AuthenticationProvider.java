124e47512de7b51da0604fcc559c1919b378a94a
zhangqi <anmiezata@163.com>
2021-09-14 23:35:05 +0800

    第一次提交

--- hoppinzq-service/src/main/java/com/hoppinzq/service/auth/AuthenticationProvider.java
+++ hoppinzq-service/src/main/java/com/hoppinzq/service/auth/AuthenticationProvider.java
@@ -8,9 +8,9 @@ import com.hoppinzq.service.exception.AuthenticationFailedException;
  * 通过提供的调用请求对调用方进行身份验证
  * 不抛出AuthenticationFailedException异常表示校验通过
  * 目前提供三种验证方式：
- * 1、AuthenticationNotRequiredAuthenticator 不进行验证
- * 2、DatabaseUsernamePasswordAuthenticator 使用数据库内的用户名密码匹配校验
- * 3、SingleUsernamePasswordAuthenticator 使用用户名密码组合的方式校验
+ * 1、AuthenticationNotCheckAuthenticator 不进行验证
+ * 2、DbUserCheckAuthenticator 使用数据库内的用户名密码匹配校验
+ * 3、SimpleUserCheckAuthenticator 使用用户名密码组合的方式校验
  * 每个服务在注册的时候会进行服务包装，通过包装服务的serviceWrapper.setAuthenticationProvider 设置校验手段
  */
 public interface AuthenticationProvider {
