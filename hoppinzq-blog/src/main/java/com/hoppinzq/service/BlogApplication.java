14ae0543c7ff2ff3e69a78bf5f4750bc21c03ce1
zhangqi <anmiezata@163.com>
2021-11-16 17:27:11 +0800

    添加博客服务

---
+++ hoppinzq-blog/src/main/java/com/hoppinzq/service/BlogApplication.java
@@ -0,0 +1,17 @@
+package com.hoppinzq.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@SpringBootApplication
+@ServletComponentScan
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
