5679fc58d654fbd7d5a8d21b76f577df7deabcb9
zhangqi <anmiezata@163.com>
2022-01-26 02:23:11 +0800

    重做滚动校验

---
+++ hoppinzq-auth/src/main/resources/static/js/buttonLoading.js
@@ -0,0 +1,44 @@
+(function ($) {
+    var methods = {
+            start: function () {
+                let loda = getData(this);
+                if (loda) loda.start();
+            },
+            stop: function () {
+                let loda = getData(this);
+                if (loda) loda.stop();
+            }
+        },
+        getData = function (el) {
+            return el.data('loda-button-data');
+        },
+        setData = function (el, data) {
+            el.data('loda-button-data', data);
+        },
+        buttonLoading = function (element) {
+            this.me = element;
+            this.$me = $(element);
+            if (getData(this.$me)) return;
+            setData(this.$me, this);
+        };
+    buttonLoading.prototype = {
+        start: function () {
+            this.$me.data("loadingq", this.$me.text().trim()).addClass("animated-bg").addClass("loading-black").attr("disabled", "disabled").css("pointer-events","none").html("<i class='fa fa-refresh fa-spin'></i>等待中");
+        },
+        stop: function () {
+            this.$me.removeClass("animated-bg").removeClass("loading-black").removeAttr("disabled").html(this.$me.data("loadingq")).css("pointer-events","all");
+        }
+    };
+    $.fn.buttonLoading = function (options) {
+        if (methods[options]) {
+            return methods[options].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof options === 'object' || !options) {
+            return this.each(function () {
+                new buttonLoading(this);
+                return this;
+            });
+        } else {
+            $.error('方法 ' + options + '不存在');
+        }
+    };
+}(jQuery));
